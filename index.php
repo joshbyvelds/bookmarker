7dd0c163bfdb6ea2121ee9d4b60031cb3e8b1258
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-02-15 20:18:42 -0500

    setup twig

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+$loader = new \Twig_Loader_Filesystem(__DIR__.'/view');
+$twig = new \Twig_Environment($loader);
+
+// Twig vars go here. :)
+echo $twig->render('index.twig',
+    [
+        'title' => 'Bookmarker'
+    ]
+);
