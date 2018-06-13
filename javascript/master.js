53d1a1dbc12da1808c5fe196526645eb1d3e6c01
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-06-12 23:23:21 -0400

    Added sample bookmarks based of zoe effect and setup desktop grid. created left hand icon nav

--- javascript/master.js
+++ javascript/master.js
@@ -1,1 +1,18 @@
 //# sourceMappingURL=master.js.map
+
+$(document).ready(init);
+
+function init() {
+    $("#leftside_panel").on('mouseenter', function(){
+        $("#leftside_panel").animate({width:"175px"}, 500);
+        $("#the_grid").animate({width:"1720px"}, 500, function(){
+            $(".icon_word-js").fadeIn(250);
+        });
+    });
+
+    $("#leftside_panel").on('mouseleave', function(){
+        $("#leftside_panel").animate({width:"55px"}, 500);
+        $("#the_grid").animate({width:"1848px"}, 500);
+        $(".icon_word-js").hide();
+    });
+}
