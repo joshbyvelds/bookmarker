29e58c94e4d5a8c5e0ffc84cfd8e37a562111495
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-06-13 21:17:29 -0400

    Fixed white flash glitch when hovering over icons

--- javascript/master.js
+++ javascript/master.js
@@ -8,8 +8,8 @@ function init() {
     $("#leftside_panel").on('mouseenter', function(){
         if(slidelock){return;}else{slidelock = true;}
         var left_width = 175;
-        var grid_width = $(window).width() - left_width;
-        $("#leftside_panel").animate({width:left_width + "px"}, 500);
+        var grid_width = $(window).width() - left_width - 2;
+        $("#leftside_panel").width($("#leftside_panel").width() - 2).animate({width:left_width + "px"}, 500);
         $("#the_grid").animate({width:grid_width + "px"}, 500, function(){
             $(".icon_word-js").fadeIn(250);
             slidelock = false;
