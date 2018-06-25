18199877f074fbfb651bb9897f3e8c27a4b2f503
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-06-24 23:08:03 -0400

    Added icon to LHS and Favicon

--- javascript/master.js
+++ javascript/master.js
@@ -9,6 +9,7 @@ function init() {
         if(slidelock){return;}else{slidelock = true;}
         var left_width = 175;
         var grid_width = $(window).width() - left_width - 2;
+        $("#left_panel_logo").fadeOut(500, function(){$(this).attr("src", "img/logo_med.png").fadeIn(500)});
         $("#leftside_panel").width($("#leftside_panel").width() - 2).animate({width:left_width + "px"}, 500);
         $("#the_grid").animate({width:grid_width + "px"}, 500, function(){
             $(".icon_word-js").fadeIn(250);
@@ -20,6 +21,7 @@ function init() {
         if(slidelock){return;}else{slidelock = true;}
         var left_width = 55;
         var grid_width = $(window).width() - left_width;
+        $("#left_panel_logo").fadeOut(500, function(){$(this).attr("src", "img/logo_small.png").fadeIn(500)});
         $("#leftside_panel").animate({width:left_width + "px"}, 500);
         $("#the_grid").animate({width: grid_width + "px"}, 500, function(){
             $("#the_grid").attr("style", "");
