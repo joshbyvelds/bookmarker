75fb435b29509aedf77c1f20356171ab2057bbf4
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-06-30 20:35:23 -0400

    Created Login page.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //Get all of our good Ol' composer stuff
 require_once("vendor/autoload.php");
 
-// Setup Twig cuz it's you know Awesome..
+// Setup Twig cuz it's..  you know.. Awesome..
 $loader = new \Twig_Loader_Filesystem(__DIR__.'/view');
 $twig = new \Twig_Environment($loader);
 
@@ -12,13 +12,29 @@ $title = "Bookmarker! - The Ultimate Bookmark Organizer";
 // Check if db file exists..
 if(!file_exists ( 'php/db.inc.php' )) {
     // If not, load install page..
-    echo $twig->render('install.twig', ['title' => $title]);
+    echo $twig->render('install.twig', ['title' => 'Install - Bookmarker!']);
     exit();
 }
 
-//TODO:: Check if user is logged in..
-$loggedIn = false;
+// If Login page..
+if(isset($_GET["page"]) && $_GET["page"] === "login"){
+    echo $twig->render('login.twig', ['title' => 'Login - Bookmarker!']);
+    exit();
+}
 
+// If Register page..
+if(isset($_GET["page"]) && $_GET["page"] === "register"){
+    echo $twig->render('register.twig', ['title' => 'Register - Bookmarker!']);
+    exit();
+}
+
+
+session_start();
+// If Index Page..
+$loggedIn = isset($_SESSION['username']);
+
+
+var_dump($loggedIn);
 //TODO:: Get user config if logged in else load public/guest bookmarks
 
 //TODO:: setup bookmarks
---
+++ php/login.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once("db.inc.php");
+
+$json = [];
+
+$json['error'] = false;
+
+if(isset($_POST['login_username'])){
+    $login_username = $_POST['login_username'];
+}
+
+if(isset($_POST['login_password'])){
+    $login_password = $_POST['login_password'];
+}
+
+
+if(empty($login_username)){
+    $json['error'] = true;
+    $json['login_username_error'] = "Please enter the username for admin account.";
+}
+
+if(empty($login_password)){
+    $json['error'] = true;
+    $json['login_password_error'] = "Please create a password for admin account.";
+}
+
+if($json['error']){
+    echo json_encode($json);
+}
+
+
+//Create Database
+$host="localhost";
+$root="root";
+
+    try {
+        $result = $db->prepare("SELECT username, password FROM users WHERE username = ?");
+        $result->bindParam(1, $login_username);
+        $result->execute();
+
+        $user = $result->fetchAll();
+        if(count($user) === 1){
+
+            if(password_verify($login_password, $user[0]['password'])){
+                session_start();
+                $_SESSION['username'] = $login_username;
+            }else{
+                $json['error'] = true;
+                $json['login_password_error'] = "username and password do not match. Try again.";
+            }
+
+            echo json_encode($json);
+        }
+
+    } catch (PDOException $e) {
+        die("DB ERROR: ". $e->getMessage());
+    }
+?>
+
+
